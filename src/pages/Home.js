7c5c53b5a9d4c540fd099b3a51c9a67a6153746b
KaiserIQ <mauricio.orrego005@pascualbravo.edu.co>
2024-11-08 22:38:42 -0500

    Create Home.js

---
+++ src/pages/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Home() {
+  return (
+    <div>
+      <h2>Home Page</h2>
+      <p>Bienvenido a mi app profe jajaja xd!</p>
+    </div>
+  );
+}
+
+export default Home;
