acd2018e91f655893794962f1d6f1c9f892d2c1e
KaiserIQ <mauricio.orrego005@pascualbravo.edu.co>
2024-11-08 22:38:39 -0500

    Create About.js

---
+++ src/pages/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function About() {
+  return (
+    <div>
+      <h2>About Page</h2>
+      <p>Esta pagina muestra nomás lo que está leyendo xd!</p>
+    </div>
+  );
+}
+
+export default About;
